f79a04522f329dc6cac69cb9764556ce0de56f0c
Ger <germanzittermann@gmail.com>
2022-04-22 04:10:29 -0300

    Notes and Recipe models created

---
+++ src/main/java/recipe/recipesspring/model/Notes.java
@@ -0,0 +1,45 @@
+package recipe.recipesspring.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    /* By default a String cannot have more than 255 characters
+    * With the Lob annotation we avoid that limitation problem
+    * */
+    @Lob
+    private String recipeNotes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+
+}
