e1f4b6b2c017d3948492248efcdad3b78278f618
Germán Zittermann <germanzittermann@gmail.com>
2022-04-23 01:42:38 -0300

    Recipe repository refactored

--- src/main/java/recipe/recipesspring/repositories/RecipeReposiroty.java
+++ src/main/java/recipe/recipesspring/repositories/RecipeReposiroty.java
@@ -3,5 +3,10 @@ package recipe.recipesspring.repositories;
 import org.springframework.data.repository.CrudRepository;
 import recipe.recipesspring.model.Recipe;
 
+import java.util.Optional;
+
 public interface RecipeReposiroty extends CrudRepository<Recipe, Long> {
+
+    Optional<Recipe> findByDescription(String description);
+
 }
